c5690417bd4f3b69dd35f166959577161934ee00
jaceuk <jnewington@gmail.com>
2024-01-04 14:59:36 +0000

    Finalised cron times

--- scraper/index.js
+++ scraper/index.js
@@ -12,9 +12,9 @@ const CHECKATRADE_ACCOUNTS = [
 
 // * minute * hour * day of month * month * day of week
 // const TIME = '0 1 * * 1';
-const TIME_1 = '40 14 * * *';
-const TIME_2 = '45 14 * * *';
-const TIME_3 = '50 14 * * *';
+const TIME_1 = '10 1 * * 1';
+const TIME_2 = '20 1 * * 1';
+const TIME_3 = '30 1 * * 1';
 
 // send email after successful import
 async function sendImportReport(checkatradeAccount, result) {
