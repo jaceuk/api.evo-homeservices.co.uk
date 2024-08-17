1973ac0752a39f733904404e646a9e84ea1a6e0d
jaceuk <jnewington@gmail.com>
2024-08-17 10:00:44 +0100

    Fixed SMTP error regarding self signed ssl certificates

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -14,6 +14,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 // verify connection configuration
--- scraper/sendEmail.js
+++ scraper/sendEmail.js
@@ -1,21 +1,31 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
-async function sendEmail(from, to, subject, html, replyTo, bcc ) {
-    let host, port, user, pass;
+async function sendEmail(from, to, subject, html, replyTo, bcc) {
+  let host, port, user, pass;
 
-    const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS
-        }
-    });
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
 
-    const result = await transporter.sendMail({ from, to, subject, html, replyTo, bcc });
-    return result;
+  const result = await transporter.sendMail({
+    from,
+    to,
+    subject,
+    html,
+    replyTo,
+    bcc,
+  });
+  return result;
 }
 
 module.exports = sendEmail;
